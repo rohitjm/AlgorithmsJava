fa5f7b5c072dace2257f81e5c8ba82c1ce894bc2
Rohit Mathew <rohit@rabb.it>
2018-05-29 09:32:15 -0700

    Fix NQueens Code and start to change for all possible solutions

--- Recursion/NQueens.java
+++ Recursion/NQueens.java
@@ -1,44 +1,50 @@
+import java.util.*;
+
 public class NQueens {
+
+	public static ArrayList<int[]> results = new ArrayList<int[]>();
+
 	public static void main(String[] args) {
 		System.out.println("NQueens Bro");
-		int[] arr = nQueensMain(4);
-		printArrayOnOneLine(arr, 0);
+		nQueensMain(4);
+		printResults(results);
 	}
 
-	static int[] nQueensMain(int n) {
+	static void nQueensMain(int n) {
 		int[] placement = new int[n];
 		nQueens(placement, 0);
-		return placement;	
+		return;	
 	}
 
 	static boolean nQueens(int[] placement, int r) {
 		if(r == placement.length) {
+			results.add(placement);
 			return true;
 		}
 		// iterate through possible placements for the column
 		for(int i = 0; i < placement.length; i++) {
-			if((ifSafe(placement, r, i)) && nQueens(placement, r++)) {
-				return false;
-			} else {
-				return false;
-			}
+			placement[r] = i;
+			boolean isSafe = ifSafe(placement, r);
+			if(isSafe && nQueens(placement, r+1)) {
+				return true;
+			} 
 		}
 		return false;
 	}
 
 	//check if the potential placement is safe
-	static boolean ifSafe(int[] placement, int row, int column) {
+	static boolean ifSafe(int[] placement, int row) {
 		for(int j = 0; j < row; j++) {
-			if(placement[j] == column) {
+			if(placement[j] == placement[row]) {
 				return false;
 			}
-			int columnDiff = column - placement[j];
+			int columnDiff = placement[row] - placement[j];
 			int rowDiff = row - j;
 			if(columnDiff == rowDiff) {
 				return false;
 			}
 		}
-		return false;	
+		return true;	
 	}
 
 	static void printArrayOnOneLine(int[] arr, int x) {
@@ -46,5 +52,13 @@ public class NQueens {
 		for(int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]);	
 		}		
+	}
+
+	static void printResults(ArrayList<int[]> results) {
+		String[] resultsArr = new String[results.size()];
+		for(int i = 0; i < results.size(); i++) {
+			int[] thisResult = results.get(i);
+			printArrayOnOneLine(thisResult, 0);
+		}
 	}
 }
